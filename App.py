dc26380ef9695d26fae04b0af68d6a27cb7aaf6d
amelmochi <novaivtihal123@students.unnes.ac.id>
2025-06-01 14:24:05 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import pandas as pd
+import pickle
+import joblib
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.title("Hello, World!")
-st.write("Ini adalah aplikasi pertama saya menggunakan Streamlit.")
+st.set_page_config(page_title="Restaurant Dashboard App", layout="centered")
+st.sidebar.header("Dashboard")
+
+st.title("Selamat datang di aplikasi streamlit sederhana")
+st.write("Aplikasi ini dibuat untuk demonstrasi projek akhir Data Mining.")
+
+#Load Dataset
+df = pd.read_csv("model/resto.csv")
+
+#Tampilkan dataframe
+st.subheader("Dataset Resto")
+st.dataframe(df)
+
+st.write(df.columns.tolist())
+#df[target] = data target
